45b26ca7047032655fa31f8c9cf4470f902cfc99
LamTP3 <phuclamtran2810@gmail.com>
2024-05-30 14:57:50 +0700

    1. connection pool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,21 @@
+const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-    return res.send('Hello World !')
-}
+  // simple query
+  let user = [];
+  connection.query("SELECT * FROM Users u ", function (err, results, fields) {
+    user = results;
+    console.log(">>>>Result: ", results); // results contains rows returned by server
+    console.log(">>>>Fields: ", fields); // fields contains extra meta data about results, if available
+    console.log("Check user: ", user);
+    return res.send(JSON.stringify(user));
+  });
+};
 
 const getSamplePage = (req, res) => {
-    return res.render('sample.ejs')
-}
+  return res.render("sample.ejs");
+};
 module.exports = {
-    getHomePage,
-    getSamplePage
-}
+  getHomePage,
+  getSamplePage,
+};
--- src/server.js
+++ src/server.js
@@ -1,32 +1,21 @@
-const express = require('express') // common js
-const configViewEngine = require('./config/viewEngine')
-const initWebRouter = require('./routes/web')
+const express = require("express"); // common js
+const configViewEngine = require("./config/viewEngine");
+const initWebRouter = require("./routes/web");
 
-require('dotenv').config()
-const connection = require('./config/database')
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 6969
-const hostname = process.env.HOST_NAME || 'localhost'
+const app = express();
+const port = process.env.PORT || 6969;
+const hostname = process.env.HOST_NAME || "localhost";
 
 // config view engine
-configViewEngine(app)
+configViewEngine(app);
 
 //test
 
 // declare routes
-app.use(`/v1`, initWebRouter)
+app.use(`/v1`, initWebRouter);
 
-
-
-// simple query
-connection.query(
-    'SELECT * FROM Users u ',
-    function (err, results, fields) {
-      console.log(">>>>Result: ",results); // results contains rows returned by server
-      console.log(">>>>Fields: ",fields); // fields contains extra meta data about results, if available
-    }
-  );
 app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
